37410fce66d7ba3132dc548d698dad238a9f203c
Horgeon <me@horgeon.fr>
2021-05-31 20:14:09 +0200

    Updated included player listener to the new section model, registering players to the section system

--- plugin/src/main/java/fr/prodrivers/bukkit/commons/sections/SectionListener.java
+++ plugin/src/main/java/fr/prodrivers/bukkit/commons/sections/SectionListener.java
@@ -1,16 +1,12 @@
 package fr.prodrivers.bukkit.commons.sections;
 
-import fr.prodrivers.bukkit.commons.exceptions.IllegalSectionLeavingException;
-import fr.prodrivers.bukkit.commons.exceptions.NoCurrentSectionException;
-import fr.prodrivers.bukkit.commons.plugin.Main;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.logging.Level;
 
 public class SectionListener implements Listener {
 	SectionListener( JavaPlugin plugin ) {
@@ -21,17 +17,13 @@ public class SectionListener implements Listener {
 	public void onPlayerJoin( PlayerJoinEvent e ) {
 		final Player player = e.getPlayer();
 
-		SectionManager.enter( player, MainHub.name );
+		SectionManager.enter(player, SectionManager.ROOT_NODE_NAME);
 	}
 
 	@EventHandler
 	public void onPlayerQuit( PlayerQuitEvent e ) {
-		final Player player = e.getPlayer();
+		final OfflinePlayer player = e.getPlayer();
 
-		try {
-			SectionManager.leave( player );
-		} catch( NoCurrentSectionException | IllegalSectionLeavingException ex ) {
-			Main.logger.log( Level.SEVERE, "Exception occurred when making the player leave as he disconnects. Player may be left in an undefined state.", ex );
-		}
+		SectionManager.unregister(player);
 	}
 }
