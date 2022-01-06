838d3a10a0c4713c13444fe88216f071dfb48142
Horgeon <me@horgeon.fr>
2022-01-06 22:54:04 +0100

    Added small warning on multiple database use in EbeanProvideer documentation

--- api/src/main/java/fr/prodrivers/bukkit/commons/storage/EbeanProvider.java
+++ api/src/main/java/fr/prodrivers/bukkit/commons/storage/EbeanProvider.java
@@ -2,6 +2,8 @@ package fr.prodrivers.bukkit.commons.storage;
 
 import fr.prodrivers.bukkit.commons.di.guice.ProdriversCommonsGuiceModule;
 import io.ebean.Database;
+import io.ebean.Model;
+import io.ebean.Transaction;
 
 import javax.inject.Provider;
 import javax.inject.Singleton;
@@ -19,6 +21,10 @@ import java.util.List;
  * offer their own provider for DatabaseConfig. Your DatabaseConfig provider should, at least, register all classes
  * (entities, ...) that you intend to use with Ebean, using for example the
  * {@link io.ebean.config.DatabaseConfig#setClasses(List) setClasses} method.
+ * <p>
+ * Important: <b>do not use</b> {@link Model#save()} or {@link Model#save(Transaction)} as it will use the first
+ * registered database instead of the one you fetched the bean from. This will cause issues when having multiple plugins
+ * using Ebean.
  * <p>
  * EbeanProvider is an optional part of the plugin, meaning that the result of its methods, on top of the underlying SQL
  * database internal quirks and wrong queries, is not guaranteed to be correct.  One must check for errors when
