ff7a793aa8b55f4580f198dc19b702016f258b9a
Horgeon <me@horgeon.fr>
2022-12-29 20:02:21 +0100

    Added instance of MiniMessageto plugin's dependency injection

--- spigot/src/main/java/fr/prodrivers/minecraft/spigot/commons/plugin/PluginModule.java
+++ spigot/src/main/java/fr/prodrivers/minecraft/spigot/commons/plugin/PluginModule.java
@@ -3,6 +3,7 @@ package fr.prodrivers.minecraft.spigot.commons.plugin;
 import com.google.inject.AbstractModule;
 import fr.prodrivers.minecraft.commons.configuration.Configuration;
 import fr.prodrivers.minecraft.commons.configuration.Messages;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -23,5 +24,6 @@ public class PluginModule extends AbstractModule {
 		bind(Configuration.class).to(EConfiguration.class);
 		bind(Messages.class).to(EMessages.class);
 		bind(DependenciesClassLoaderProvider.class).toInstance(new DependenciesClassLoaderProvider(this.classLoader));
+		bind(MiniMessage.class).toInstance(MiniMessage.miniMessage());
 	}
 }
